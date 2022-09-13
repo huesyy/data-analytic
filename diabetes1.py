da4c655fe7fef8a58e5692c14d4aceecdba44344
huesyy <113413239+huesyy@users.noreply.github.com>
2022-09-13 22:45:40 +0800

    Update diabetes1.py

--- diabetes1.py
+++ diabetes1.py
@@ -1,9 +1,5 @@
 import pandas as pd
 df = pd.read_csv('Diabetes.csv')
-df.head()
-
-sns.heatmap(df.corr(),annot=True)
-sns.pairplot(df,hue='Outcome')
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
