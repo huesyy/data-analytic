d7868556ae38a486fe63833438ce13f6c3bc2511
huesyy <113413239+huesyy@users.noreply.github.com>
2022-09-14 11:48:33 +0800

    Update mall_customer2.py

--- mall_customer2.py
+++ mall_customer2.py
@@ -7,23 +7,18 @@ df = pd.read_csv(mall_customer.csv)
 features = ['Annual_Income_(k$)', 'Spending_Score']
 X = df[features]
 
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots(X['Annual_Income_(k$)'], X['Spending_Score'])
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
 
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters=5)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
 arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
-
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
